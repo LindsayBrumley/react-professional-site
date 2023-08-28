834ccdfef356bf5b955434f80ae2738a3c773610
LindsayBrumley <lalainaphillips@gmail.com>
2023-08-28 18:38:13 -0400

    components reconfigured, icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "../src/Components/Home";
 import { About } from "../src/Components/About";
 import { Portfolio } from "../src/Components/Portfolio";
 import { Contact } from "../src/Components/Contact";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  const navigate = useNavigate();
-
   return (
     <div>
-      <header className="header">
-        <h1 className="title">Lindsay Brumley</h1>
-
+      <nav>
         <div className="links">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/portfolio">Portfolio</Link>
           <Link to="/contact">Contact</Link>
         </div>
-      </header>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <footer className="footer">
-        <div>
-          <a
-            href="https:github.com/LindsayBrumley"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="github"
-          >
-            <FontAwesomeIcon icon={faGithub} /> Github
-          </a>
-        </div>
-      </footer>
+
+      <footer className="footer"></footer>
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 export function Home() {
-  return <div className="body"></div>;
+  return (
+    <div>
+      <h1 className="title">Lindsay Brumley</h1>
+      <img
+        src="https://drive.google.com/uc?export=view&id=1ys19YeVXbvzDgEBh8JUdEZop8k_-bBIE"
+        className="headshot"
+      />
+      <div>
+        <a
+          href="https:github.com/LindsayBrumley"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github"
+        >
+          <FontAwesomeIcon icon={faGithub} size="4x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lindsaybrumley"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkedin"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="4x" />
+        </a>
+      </div>
+    </div>
+  );
 }
