8a57325b2e840187418d3487a8418e94038803bc
LindsayBrumley <lalainaphillips@gmail.com>
2023-08-30 17:29:14 -0400

    changed home page style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,15 +7,21 @@ export function Home() {
   return (
     <div className="home-page">
       <div className="home-container">
-        <div className="headshot">
-          <img
-            src="https://drive.google.com/uc?export=view&id=1ys19YeVXbvzDgEBh8JUdEZop8k_-bBIE"
-            alt="Lindsay Brumley Headshot"
-            className="photo"
-          />
+        <div className="frame-container">
+          <div className="headshot">
+            <img
+              src="https://drive.google.com/uc?export=view&id=1ys19YeVXbvzDgEBh8JUdEZop8k_-bBIE"
+              alt="Lindsay Brumley Headshot"
+              className="photo"
+            />
+          </div>
         </div>
         <div className="title-container">
-          <h1 className="title">Lindsay Brumley</h1>
+          <h1 className="title">
+            <a href="/about" className="name-link">
+              Lindsay Brumley
+            </a>
+          </h1>
           <p className="description">Junior Software Engineer</p>
         </div>
       </div>
@@ -24,7 +30,7 @@ export function Home() {
           href="https:github.com/LindsayBrumley"
           target="_blank"
           rel="noopener noreferrer"
-          className="github"
+          className="github social-icon"
         >
           <FontAwesomeIcon icon={faGithub} size="4x" />
         </a>
@@ -32,7 +38,7 @@ export function Home() {
           href="https://www.linkedin.com/in/lindsaybrumley"
           target="_blank"
           rel="noopener noreferrer"
-          className="linkedin"
+          className="linkedin social-icon"
         >
           <FontAwesomeIcon icon={faLinkedin} size="4x" />
         </a>
@@ -40,3 +46,49 @@ export function Home() {
     </div>
   );
 }
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGithub } from "@fortawesome/free-brands-svg-icons";
+// import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+// import "../Components/component-css/home.css";
+
+// export function Home() {
+//   return (
+//     <div className="home-page">
+//       <div className="home-container">
+//         <div className="headshot">
+//           <figure className="snip1113 white">
+//             <img
+//               src="https://drive.google.com/uc?export=view&id=1ys19YeVXbvzDgEBh8JUdEZop8k_-bBIE"
+//               alt="Lindsay Brumley Headshot"
+//               className="photo"
+//             />
+//             <figcaption>
+//               <h3>
+//                 Lindsay <span>Brumley</span>
+//               </h3>
+//               <h4>Junior Software Engineer</h4>
+//             </figcaption>
+//           </figure>
+//         </div>
+//       </div>
+//       <footer>
+//         <a
+//           href="https:github.com/LindsayBrumley"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="github"
+//         >
+//           <FontAwesomeIcon icon={faGithub} size="4x" />
+//         </a>
+//         <a
+//           href="https://www.linkedin.com/in/lindsaybrumley"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="linkedin"
+//         >
+//           <FontAwesomeIcon icon={faLinkedin} size="4x" />
+//         </a>
+//       </footer>
+//     </div>
+//   );
+// }
