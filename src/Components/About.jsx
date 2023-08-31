8c287230ed5b2039d4da84e11bf64011fe0e0335
LindsayBrumley <lalainaphillips@gmail.com>
2023-08-30 20:02:44 -0400

    styling to contact info mostly done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,40 @@
 export function About() {
-  return <header>About</header>;
+  return (
+    <div className="wrapper">
+      <div className="item">
+        <div className="polaroid">
+          <img src="https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg" />
+          <div className="caption">I Miss London</div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="polaroid">
+          <img src="https://image.ibb.co/mmyvrc/anniversary_balloons_birthday_68369.jpg" />
+          <div className="caption">Love ballons</div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="polaroid">
+          <img src="https://image.ibb.co/hQaarc/antique_blur_camera_828378.jpg" />
+          <div className="caption">Vintage life </div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="polaroid">
+          <img src="https://image.ibb.co/crFarc/pexels_photo_100756.jpg" />
+          <div className="caption">Summer Day</div>
+        </div>
+      </div>
+
+      <div className="item">
+        <div className="polaroid">
+          <img src="https://image.ibb.co/fvekrc/action_adult_art_673649.jpg" />
+          <div className="caption">Art</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,48 +1,32 @@
 import "../Components/component-css/contact.css";
+
 export function Contact() {
   return (
-    // <div>
-    //   <header>Contact Information</header>
-    //   <figure className="fig1">
-    //     <img
-    //       src="https://drive.google.com/uc?export=view&id=10uLrNKJMFFjnn_QihwYuQvuaL2xS_JU8"
-    //       classNameName="prophoto"
-    //     />
-    //   </figure>
-    // </div>
-    <div className="wrapper">
-      <div className="item">
-        <div className="polaroid">
-          <img src="https://image.ibb.co/b8UJBc/administration_architecture_big_ben_221166.jpg" />
-          <div className="caption">I Miss London</div>
+    <div>
+      <header>Hit me up</header>
+      <div className="container">
+        <div className="contact-info">
+          <a
+            href="mailto:lindsay.a.brumley@gmail.com?subject=Subject%20Here&body=Hello%20there"
+            className="contact-links"
+          >
+            Send me an email
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lindsaybrumley/"
+            className="contact-links"
+          >
+            Linkedin
+          </a>
+          <a href="https://github.com/LindsayBrumley" className="contact-links">
+            GitHub
+          </a>
         </div>
-      </div>
-
-      <div className="item">
-        <div className="polaroid">
-          <img src="https://image.ibb.co/mmyvrc/anniversary_balloons_birthday_68369.jpg" />
-          <div className="caption">Love ballons</div>
-        </div>
-      </div>
-
-      <div className="item">
-        <div className="polaroid">
-          <img src="https://image.ibb.co/hQaarc/antique_blur_camera_828378.jpg" />
-          <div className="caption">Vintage life </div>
-        </div>
-      </div>
-
-      <div className="item">
-        <div className="polaroid">
-          <img src="https://image.ibb.co/crFarc/pexels_photo_100756.jpg" />
-          <div className="caption">Summer Day</div>
-        </div>
-      </div>
-
-      <div className="item">
-        <div className="polaroid">
-          <img src="https://image.ibb.co/fvekrc/action_adult_art_673649.jpg" />
-          <div className="caption">Art</div>
+        <div className="container-frame">
+          <img
+            src="https://drive.google.com/uc?export=view&id=10uLrNKJMFFjnn_QihwYuQvuaL2xS_JU8"
+            className="power-stance"
+          ></img>
         </div>
       </div>
     </div>
