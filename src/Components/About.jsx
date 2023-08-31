fc0a456891fcf6e13ffa7476f57dce08ac5e065b
LindsayBrumley <lalainaphillips@gmail.com>
2023-08-31 19:21:35 -0400

    added photos to the about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,16 +3,48 @@ import "../Components/component-css/about.css";
 export function About() {
   return (
     <div>
-      <div className="about-title">About</div>
+      <div className="about-title">About me</div>
       <div className="about-container">
         <p className="p-1"></p>
-        <img src="" />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1JHSxWFeExo2U_MAs8hDw8Wb-ZEB8YUQ-"
+          className="austin"
+        />
         <p className="p-2"></p>
-        <img src="" />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1CCjIvQYzAojVAy8AwQriehQHuVMbRiss"
+          className="bigluke"
+        />
+        {/* <img
+          src=""
+          className="family"
+        /> */}
         <p className="p-3"></p>
-        <img src="" />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1cKojhprXwQfXCDwJmFbdDwLSsi21xJF3"
+          className="boulder1"
+        />
         <p className="p-4"></p>
-        <img src="" />
+        <img
+          src="https://drive.google.com/uc?eport=view&id=14288WU2X03uTNevkXA8b64ntyKiKvYW_"
+          className="boulder2"
+        />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1LQzBjs4FCT54pCZePj223TgjFUt_5DOy"
+          className="guitar"
+        />
+        <img
+          src="https://drive.google.com/uc?export-view&id=1l1KUGvD02VoVXr2Kobb-8cQH4RgrVG3f"
+          className="keys"
+        />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1LIL8nERvoVf0Ln4sNVkTD9iZd0wZ9n6e"
+          className="salon"
+        />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1cRHUCOyFJpueKmDI93eTvTILXTg8mE9y"
+          className="view"
+        />
       </div>
     </div>
   );
